8e15705b499dd7cf25c149127afccb02cc5ca13f
Ahmed-Elzaky <A7med3lzeky@outlook.com>
2022-11-09 21:00:17 +0200

    modified header and shuffle

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,43 @@
 'use strict';
 
+// navbar ul
 $('nav ul li').click(function () {
   $(this).addClass('active').siblings('li').removeClass('active');
+  let offsetSec = $('.' + $(this).data('class')).offset().top - 20;
+  $('body, html').animate ({
+    scrollTop: offsetSec,
+  }, 1000)
 })
 
-$('.landing .bullets li').click(function () {
-  $(this).addClass('active').siblings('li').removeClass('active');
-})
-
+//toggle button at small screen
 $('nav .toggle-menu').click(function () {
   $(this).toggleClass('active-bars');
   $('nav .toggle-menu + ul').toggleClass('hide-ul');
 })
 
-$('.portfolio ul li').click(function () {
+// scroll to section
+$(window).scroll(function () {
+  let scrolly = $(window).scrollTop();
+
+  if (scrolly >= $('.landing').height() - 50 ) {
+    $('header').addClass('header-scroll');
+  }
+  else {
+    $('header').removeClass('header-scroll');
+  }
+
+  $(' header nav ul').addClass('hide-ul'); // hide .nav-list if open while scroll
+
+})
+
+
+$('.landing .bullets li').click(function () {
   $(this).addClass('active').siblings('li').removeClass('active');
 })
 
-// $('.info-list li').click(function () {
-//   $().addClass('selected').siblings('li').removeClass('selected');
-//   $('.info-content > div').hide();
-//   $('.' + $(this).data('class')).fadeIn();
-// })
+// shuffle portfolio
+$('.portfolio ul li').click(function () {
+  $(this).addClass('active').siblings('li').removeClass('active');
+  $('.cards > div').hide();
+  $('.' + $(this).data('class')).fadeIn();
+})
